e872b763c4cd0a3fe43b87ea30d1c237cf0300ed
KaptainCS3 <mbiapplegryn@gmail.com>
2022-05-24 09:42:46 +0100

    input frame

---
+++ Projects/FlightReservation/Unit2.h
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit2H
+#define Unit2H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm2 : public TForm
+{
+__published:	// IDE-managed Components
+	TPanel *Panel1;
+	TLabel *Label1;
+	TLabel *Label2;
+	TEdit *Edit1;
+	TButton *Button1;
+	TEdit *Edit2;
+	TLabel *Label3;
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm2(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm2 *Form2;
+//---------------------------------------------------------------------------
+#endif
