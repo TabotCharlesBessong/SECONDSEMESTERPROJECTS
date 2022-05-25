b985fa9d907e2f396814928cefffe33c481fa971
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-05-25 11:32:24 +0100

    created admin panels and added some changes

--- FlightReservation/MainPanel.h
+++ FlightReservation/MainPanel.h
@@ -9,15 +9,16 @@
 #include <FMX.Controls.Presentation.hpp>
 #include <FMX.StdCtrls.hpp>
 #include <FMX.Types.hpp>
+#include <FMX.Objects.hpp>
 //---------------------------------------------------------------------------
 class TMainForm : public TForm
 {
 __published:	// IDE-managed Components
-	TImageControl *admin_img;
 	TButton *passenger_login_btn;
 	TButton *admin_login_btn;
 	TLabel *x;
-	TImageControl *passenger_img;
+	TImage *Image1;
+	TImage *Image2;
 	void __fastcall admin_login_btnClick(TObject *Sender);
 	void __fastcall passenger_login_btnClick(TObject *Sender);
 private:	// User declarations
